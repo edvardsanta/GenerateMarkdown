e6275810e5bf43cb7b25e1437e4fa267de0bc4c4
edvardsanta <eduardosantamarco123@outlook.com>
2024-07-20 21:20:52 -0300

    create upload files

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="flex flex-row gap-4 p-4">
     <div class="flex-1">
+      <input
+        type="file"
+        accept=".md"
+        class="mb-4 p-2 border border-gray-300 rounded-lg cursor-pointer hover:bg-gray-200 transition duration-300"
+        @change="handleFileUpload"
+      >
       <MarkdownEditor
-        class="w-full h-full"
+        class="w-full h-full bg-white p-4 border border-gray-300 rounded-lg shadow-inner"
+        :markdown-content="markdownContent"
         @update:markdown-content="handleMarkdownChange"
-     
       />
     </div>
 
@@ -16,12 +22,26 @@
 </template>
 
 <script lang="ts" setup>
-const markdownContent = ref<string>();
+import { ref } from "vue";
 
-const handleMarkdownChange = ((newContent: string) => {
-  console.log(newContent);
+const markdownContent = ref<string>("");
+
+const handleFileUpload = async (event: Event) => {
+  const input = event.target as HTMLInputElement;
+  if (!input.files?.length) return;
+
+  const file = input.files[0];
+  const reader = new FileReader();
+
+  reader.onload = (e) => {
+    const content = e.target?.result as string;
+    markdownContent.value = content;
+  };
+
+  reader.readAsText(file);
+};
+
+const handleMarkdownChange = (newContent: string) => {
   markdownContent.value = newContent;
-})
+};
 </script>
-
-<style></style>
